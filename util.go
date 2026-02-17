b9644d22d9175f950c3f09eb07d32692c5851dec
Jairo Calcina <j.calcina.v@gmail.com>
2026-02-17 03:29:49 -0300

    refactor: change code to get tasks at the beginning of execution

--- util.go
+++ util.go
@@ -7,28 +7,12 @@ import (
 	"os"
 )
 
-func GetCurrentID() int {
+func getTasks() []Task {
 	jsonData, err := os.ReadFile("tasks.json")
 	if err != nil && !errors.Is(err, os.ErrNotExist) {
 		log.Fatalf("Error reading tasks.json: %v", err)
 	} else if errors.Is(err, os.ErrNotExist) {
-		return 1
-	}
-	var tasks []Task
-	err = json.Unmarshal(jsonData, &tasks)
-	if err != nil {
-		log.Fatalf("Error parsing tasks.json: %v", err)
-	}
-	lastID := tasks[len(tasks)-1].ID
-	return lastID + 1
-}
-
-func SaveTask(task Task) {
-	jsonData, err := os.ReadFile("tasks.json")
-	if err != nil && !errors.Is(err, os.ErrNotExist) {
-		log.Fatalf("Error reading tasks.json: %v", err)
-	} else if errors.Is(err, os.ErrNotExist) {
-		jsonData = []byte("[]")
+		return []Task{}
 	}
 	var tasks []Task
 	if len(jsonData) > 0 {
@@ -37,7 +21,15 @@ func SaveTask(task Task) {
 			log.Fatalf("Error parsing tasks.json: %v", err)
 		}
 	}
-	tasks = append(tasks, task)
+	return tasks
+}
+
+func getCurrentID() int {
+	lastID := tasks[len(tasks)-1].ID
+	return lastID + 1
+}
+
+func saveTasks() {
 	updatedData, err := json.MarshalIndent(tasks, "", "  ")
 	if err != nil {
 		log.Fatalf("Error marshaling tasks: %v", err)
@@ -46,4 +38,13 @@ func SaveTask(task Task) {
 	if err != nil {
 		log.Fatalf("Error writing to tasks.json: %v", err)
 	}
+}
+
+func getTaskByID(id int) (*Task, error) {
+	for i := range tasks {
+		if tasks[i].ID == id {
+			return &tasks[i], nil
+		}
+	}
+	return nil, errors.New("task not found")
 }
